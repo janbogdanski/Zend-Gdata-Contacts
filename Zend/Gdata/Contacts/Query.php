f158198d40e65e64d8633312ce18cdf7d817402f
Jan Bogdanski <jan.b@aol.pl>
2015-02-28 20:42:12 +0100

    implementation of Zend_Gdata_Contacts_Query class

--- Zend/Gdata/Contacts/Query.php
+++ Zend/Gdata/Contacts/Query.php
@@ -1,20 +1,21 @@
 <?php
+/**
+ * @see Zend_Exception
+ */
+require_once 'Zend/Exception.php';
 
 /**
- * https://github.com/prasad83/Zend-Gdata-Contacts
- * @author prasad
- * 
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://framework.zend.com/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@zend.com so we can send you a copy immediately.
- *
- * @category   Zend
- * @package    Zend_Gdata
- * @subpackage Contacts
+ * @see Zend_Gdata_Query
  */
+require_once('Zend/Gdata/Query.php');
+
+class Zend_Gdata_Contacts_Query extends Zend_Gdata_Query
+{
+    const CONTACTS_FEED_URI = 'https://www.google.com/m8/feeds/contacts/default/full';
+
+    protected $_defaultFeedUri = self::CONTACTS_FEED_URI;
+    public function __construct($url = null)
+    {
+        parent::__construct($url);
+    }
+}
